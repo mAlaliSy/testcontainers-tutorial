0e4add047cfab0c92b6231699b1d8f6d7f566e71
Mohammad.Al-Ali <m@alali.dev>
2022-03-08 09:16:36 +0300

    Initial commit

---
+++ src/main/java/dev/alali/testcontainers/TestcontainersDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.alali.testcontainers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcontainersDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestcontainersDemoApplication.class, args);
+    }
+
+}
